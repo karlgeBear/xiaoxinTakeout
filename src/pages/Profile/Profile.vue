5358505a5384625cb2bee2cc687c18d16d08c837
karlge <karlge_bear@163.com>
2021-10-10 11:10:06 +0800

    Footer

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
     <section class="profile">
-      <header class="header">
-        <a class="header_title">
-          <span class="header_title_text">我的</span>
-        </a>
-      </header>
-      <section class="profile-number">
+      <HeaderTop title="我的"/>
+      <section class="profile-number" @click="$router.push('/login')">
         <a href="javascript:" class="profile-link">
           <div class="profile_image">
             <i class="iconfont icon-person"></i>
@@ -98,44 +94,17 @@
 </template>
 
 <script type='text/ecmascript-6'>
+import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
 export default {
-  data () {
-    return {}
-  },
+  components: {
+    HeaderTop
+  }
 }
-
 </script>
 <style scoped lang='stylus' rel='stylesheet/stylus'>
   @import '../../common/stylus/mixins.styl'
   .profile //我的
     width 100%
-    .header
-      background-color #02a774
-      position fixed
-      z-index 100
-      left 0
-      top 0
-      width 100%
-      height 45px
-      .header_search
-        position absolute
-        left 15px
-        top 50%
-        transform translateY(-50%)
-        width 10%
-        height 50%
-        .iconfont
-          font-size 22px
-          color #fff
-      .header_title
-        position absolute
-        top 50%
-        left 50%
-        transform translate(-50%, -50%)
-        width 30%
-        color #fff
-        font-size 22px
-        text-align center
     .profile-number
       margin-top 45.5px
       .profile-link
--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     <section class="search">
-      <header class="header">
-        <a class="header_title">
-          <span class="header_title_text">搜索</span>
-        </a>
-      </header>
+      <HeaderTop title="搜索"/>
       <form class="search_form" action="#">
         <input type="search" name="search" placeholder="请输入商家或美食名称" class="search_input">
         <input type="submit" name="submit" class="search_submit">
@@ -15,10 +11,11 @@
 </template>
 
 <script type='text/ecmascript-6'>
+import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
 export default {
-  data () {
-    return {}
-  },
+  components: {
+    HeaderTop
+  }
 }
 
 </script>
@@ -26,33 +23,6 @@ export default {
   @import '../../common/stylus/mixins.styl'
   .search  //搜索
     width 100%
-    .header
-      background-color #02a774
-      position fixed
-      z-index 100
-      left 0
-      top 0
-      width 100%
-      height 45px
-      .header_search
-        position absolute
-        left 15px
-        top 50%
-        transform translateY(-50%)
-        width 10%
-        height 50%
-        .iconfont
-          font-size 22px
-          color #fff
-      .header_title
-        position absolute
-        top 50%
-        left 50%
-        transform translate(-50%, -50%)
-        width 30%
-        color #fff
-        font-size 22px
-        text-align center
     .search_form
       clearFix()
       margin-top 45px
