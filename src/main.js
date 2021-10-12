43d436df356435f8fb12326a28f0355694ec38d5
karlge <karlge_bear@163.com>
2021-10-12 19:03:05 +0800

    enter file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router' 
+import store from './vuex/store'
 
 import 'lib-flexible'
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router // 所有组件都能看到 $router和$route  <router-link> 和 <router-view>
+  router, // 所有组件都能看到 $router和$route  <router-link> 和 <router-view>
+  store   // 所有组件都能看到： $store
 }).$mount('#app')
