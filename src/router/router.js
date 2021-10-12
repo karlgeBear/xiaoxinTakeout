0075c0d3578ae4a689279ed1b677ecb960c8c41d
karlge <karlge_bear@163.com>
2021-10-12 19:02:03 +0800

    update to meta

--- src/router/router.js
+++ src/router/router.js
@@ -6,23 +6,32 @@ import MSite from '@/pages/MSite/MSite.vue'
 import Search from '@/pages/Search/Search.vue'
 import Order from '@/pages/Order/Order.vue'
 import Profile from '@/pages/Profile/Profile.vue'
+import Login from '@/pages/Login/Login.vue'
 
 export default [
   {
     path: '/msite',
-    component:MSite
+    component:MSite,
+    meta: { requiresAuth: true }
   },
   {
     path: '/search',
-    component:Search
+    component:Search,
+    meta: { requiresAuth: true }
   },
   {
     path: '/order',
-    component:Order
+    component:Order,
+    meta: { requiresAuth: true }
   },
   {
     path: '/profile',
-    component:Profile
+    component:Profile,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/login',
+    component:Login
   },
   {
     path:'/',
