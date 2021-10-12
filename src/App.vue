519f088f260feea41b47b21bb58d613560d45719
karlge <karlge_bear@163.com>
2021-10-12 19:01:10 +0800

    Handle the components of the App

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
+
     <router-view></router-view>
-    <FooterGuide></FooterGuide>
+    <FooterGuide v-show="$route.meta.requiresAuth"></FooterGuide>
   </div>
 </template>
 
 <script>
-import FooterGuide from 'components/FooterGuide/FooterGuide.vue'
+import FooterGuide from "components/FooterGuide/FooterGuide.vue";
+import { mapActions } from 'vuex'
 export default {
-  components:{
-    FooterGuide
-  }
-}
+  // methods:{
+  //   触发多个action，避免一个个dispatch
+  //   ...mapActions(['getAddress','getCategorys','getShops'])
+  // },
+  mounted() {
+    // 通知action异步获取address并保存到state
+    this.$store.dispatch('getAddress')
+  },
+  components: {
+    FooterGuide,
+  },
+};
 </script>
 
-<style>
-
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+  *
+    touch-action: none; /* 当触控事件发生在元素上时，不进行任何操作。 */
 </style>
