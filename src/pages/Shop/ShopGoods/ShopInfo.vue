5c10ae6d2c06bc3e57e8310be38ac57112467f8d
karlge <karlge_bear@163.com>
2021-10-25 19:04:38 +0800

    shop components

---
+++ src/pages/Shop/ShopGoods/ShopInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    ShopInfo组件
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+  data () {
+    return {}
+  },
+}
+
+</script>
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+</style>
