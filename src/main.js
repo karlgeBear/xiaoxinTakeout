dfec84ad8da092516dddc2ae20e3ed2e701bbddb
karlge <karlge_bear@163.com>
2021-11-01 18:21:18 +0800

    update to shopCat

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import router from './router'
 import store from './vuex/store'
 import MockServe from './mock/mockServe'
 import { Button } from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
 import CartControl from './components/CartControl/CartControl.vue'
+import loading from '@/common/images/loading.gif'
 
 //全局注册组件
 Vue.component(Button.name, Button)
 Vue.component('CartControl',CartControl)
-
+// 使用插件
+Vue.use(VueLazyload,{  // 要在图片加载前显示
+  loading  // 内部自定义一个全局指令v-lazy
+})
 import 'lib-flexible'
 
 Vue.config.productionTip = false
